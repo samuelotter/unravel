edde33451a41b299899ab9e1e30042ae10328389
Samuel Otter <samuel@otter.se>
2014-11-27 00:00:53 +0100

    Build graph of events

--- src/sprof.erl
+++ src/sprof.erl
@@ -1,13 +1,8 @@
 -module(sprof).
 
--export([ analyse/1
-        %% , profile/1
-        , apply/2
-        %% , apply/3
+-export([ apply/2
         , start/1
         , stop/1
-        , start_tracing/1
-        , stop_tracing/0
         ]).
 
 %%% * API **********************************************************************
@@ -16,8 +11,10 @@ apply(Label, Fun) ->
     start_tracing(Label),
     Self = self(),
     Pid = spawn(fun() ->
-                        Result = receive trigger -> Fun() end,
-                        Self ! Result
+                        receive trigger ->
+                                Result = Fun(),
+                                Self ! Result
+                        end
                 end),
     Pid ! trigger,
     receive
@@ -35,29 +32,11 @@ start(_Options) ->
     {ok, Pid}.
 
 stop(Pid) ->
+    seq_trace:reset_trace(),
     Pid ! {stop, self()},
     receive
         {stopped, Pid} ->
             ok
-    after 5000 ->
-            timeout
-    end.
-
-start_tracing(Label) ->
-    seq_trace:set_token(label, Label),
-    seq_trace:set_token('receive', true),
-    seq_trace:set_token(timestamp, true),
-    seq_trace:set_token(send, true).
-
-stop_tracing() ->
-    seq_trace:set_token([]).
-
-analyse(Label) ->
-    Tracer = seq_trace:get_system_tracer(),
-    Tracer ! {dump, self(), Label},
-    receive
-        {reply, Events} ->
-            {ok, Events}
     after 5000 ->
             timeout
     end.
@@ -73,20 +52,64 @@ msg_handler(Events0) ->
             Events = [Msg | Events0],
             msg_handler(Events);
         {dump, From, Label} ->
-            %%Events = dump_events(Label, []),
             Events = lists:filter(fun({seq_trace, L, _, _}) ->
                                           L == Label
                                   end, Events0),
-            From ! {reply, Events},
-            msg_handler([]);
-        {stop, From} ->
-            From ! {stopped, self()}
-    end.
+            From ! {reply, lists:reverse(Events)},
+             msg_handler([]);
+         {stop, From} ->
+             From ! {stopped, self()}
+     end.
 
-%% dump_events(Label, Acc0) ->
-%%     receive
-%%         {seq_trace, Label, TraceInfo, TimeStamp} ->
-%%             Acc = [{Label, TimeStamp, TraceInfo} | Acc0],
-%%             dump_events(Label, Acc);
-%%         _ -> Acc0
-%%     end.
+ start_tracing(Label) ->
+     seq_trace:set_token(label, Label),
+     seq_trace:set_token('receive', true),
+     seq_trace:set_token(timestamp, true),
+     seq_trace:set_token(send, true).
+
+ stop_tracing() ->
+     seq_trace:set_token([]).
+
+ analyse(Label) ->
+     Tracer = seq_trace:get_system_tracer(),
+     Tracer ! {dump, self(), Label},
+     receive
+         {reply, Events} ->
+             Graph0 = digraph:new(),
+             Graph = fold_events(Graph0, Events),
+             {ok, Graph}
+     after 5000 ->
+             timeout
+     end.
+
+fold_events(Graph, []) ->
+    Graph;
+fold_events(Graph, [Event | Events]) ->
+    {seq_trace, _Label, TraceInfo, TimeStamp} = Event,
+    {Type, {PrevSerial, CurrentSerial}, From, To, Message} = TraceInfo,
+    case Type of
+        send ->
+            Previous = digraph:add_vertex(Graph, {From, PrevSerial}),
+            Current  = digraph:add_vertex(Graph, {From, CurrentSerial},
+                                          {Type, TimeStamp}),
+            digraph:add_edge(Graph, Previous, Current);
+        'receive' ->
+            Previous = digraph:add_vertex(Graph, {To, PrevSerial}),
+            Current  = digraph:add_vertex(Graph, {To, CurrentSerial},
+                                          {Type, TimeStamp}),
+            digraph:add_edge(Graph, Previous, Current),
+
+            Source = find_sender(Graph, digraph:vertices(Graph), From, Message),
+            digraph:add_edge(Graph, Source, Current, Message);
+        _   -> ok
+    end,
+    fold_events(Graph, Events).
+
+find_sender(Graph, [], From, Message) ->
+    digraph:add_vertex(Graph, {From, 0}, {send, unknown, Message});
+find_sender(Graph, [Vertex | Vertices], From, Message) ->
+    case digraph:vertex(Graph, Vertex) of
+        {{From, _}, {send, _, Message}} ->
+            Vertex;
+        _ -> find_sender(Graph, Vertices, From, Message)
+    end.
