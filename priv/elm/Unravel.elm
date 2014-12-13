ad325e01b9df6b866eedc164ace4f431b6f03d1b
Samuel Otter <samuel@otter.se>
2014-12-13 23:52:05 +0100

    Add websocket connection from client

--- priv/elm/Unravel.elm
+++ priv/elm/Unravel.elm
@@ -1,6 +1,35 @@
 module Unravel where
 
 import Html (..)
+import List (..)
+import Signal
 import Text
+import WebSocket
 
-main = Text.plainText "Welcome..."
+main = inputs "profile"
+       |> state
+       |> Signal.map view
+       |> Signal.map (toElement 400 400)
+
+type alias Event = String
+type alias State = List Event
+
+update : Input -> State -> State
+update input state = case input of
+                       ServerMessage msg -> msg :: state
+
+view : State -> Html
+view state = map (\str -> text str) state
+             |> div []
+
+state : Signal Input -> Signal State
+state input = Signal.foldp update [] input
+
+type Input = ServerMessage String
+
+server = Signal.channel ""
+
+inputs : String -> Signal Input
+inputs streamId = Signal.map ServerMessage (stream streamId (Signal.subscribe server))
+
+stream id input = WebSocket.connect ("ws://localhost:8888/stream/" ++ id) input
--- src/unravel_stream_websocket.erl
+++ src/unravel_stream_websocket.erl
@@ -1,24 +1,38 @@
 -module(unravel_stream_websocket).
+-behaviour(cowboy_http_handler).
+-behaviour(cowboy_websocket_handler).
 
--export([ init/2
+-export([ init/3
+        , handle/2
         , terminate/3
         , websocket_handle/3
         , websocket_info/3
+        , websocket_init/3
+        , websocket_terminate/3
         ]).
 
 -record(state, {}).
 
 %%% * Websocket handler callbacks ==============================================
 
-init(Req, _Opts) ->
+init(_Type, _Req, __Opts) ->
+  {upgrade, protocol, cowboy_websocket}.
 
-  {cowboy_websocket, Req, #state{}}.
+handle(_Req, State) ->
+  {noreply, State}.
 
 terminate(_Reason, _Req, _State) ->
   ok.
 
-websocket_handle(_Msg, _Req, _State) ->
-  ok.
+websocket_init(_Type, Req, _Opts) ->
+  %% TODO: Setup subscription to event stream.
+  {ok, Req, #state{}}.
 
-websocket_info(_Info, _Req, _State) ->
+websocket_handle(_Msg, Req, State) ->
+  {reply, {text, <<"Init">>}, Req, State, hibernate}.
+
+websocket_info(Info, Req, State) ->
+  {reply, {text, list_to_binary(Info)}, Req, State, hibernate}.
+
+websocket_terminate(_Reason, _, _State) ->
   ok.
