d23de8d49dd60ff79ca877ad4a1cda88cf2b68dd
Samuel Otter <samuel@otter.se>
2014-12-16 21:51:20 +0100

    List events by process

--- priv/elm/Unravel.elm
+++ priv/elm/Unravel.elm
@@ -1,13 +1,25 @@
 module Unravel where
 
+import Debug
+import Dict
 import Graphics.Element (..)
 import Html (..)
+import Json.Decode (..)
 import List (..)
+import List
+import Maybe (..)
+import Result
 import Signal
 import String
 import Text
 import WebSocket
 import Window
+
+type Input = ServerMessage (Maybe Event)
+
+type alias Event = { timestamp : Float, pid: String, eventType: String }
+type alias State = { processes: Dict.Dict String (List Event) }
+
 
 main = inputs
        |> state
@@ -17,21 +29,36 @@ main = inputs
 render : (Int, Int) -> Html -> Element
 render (w, h) html = toElement w h html
 
-type alias Event = String
-type alias State = List Event
-
 update : Input -> State -> State
-update input state = case input of
-                       ServerMessage msg -> msg :: state
+update input state =
+    case input of
+      ServerMessage (Just event) ->
+          let processes =
+                  case Dict.get event.pid state.processes of
+                    Just events ->
+                        Dict.insert event.pid (event :: events) state.processes
+                    Nothing ->
+                        Dict.insert event.pid [event] state.processes
+          in
+            { state | processes <- processes }
+      _ ->
+          Debug.log "Unhandled message" state
 
 view : State -> Html
-view state = map (\str -> p [] [text str]) state
-             |> div []
+view state =
+    Dict.toList state.processes
+        |> List.map processView
+        |> div []
+
+processView : (String,  (List Event)) -> Html
+processView (pid, events) =
+    div [] ((text pid) :: List.map (.eventType >> text) events)
+
+initialState = { processes = Dict.empty }
 
 state : Signal Input -> Signal State
-state input = Signal.foldp update [] input
+state input = Signal.foldp update initialState input
 
-type Input = ServerMessage String
 
 server = Signal.channel ""
 
@@ -42,7 +69,17 @@ stream input =
     let base = case location of
                  Just str -> String.dropLeft 4 str
                  Nothing  -> ""
-    in
+     in
       WebSocket.connect ("ws" ++ base ++ "/stream") input
+          |> Signal.map (decodeString eventDecoder)
+          |> Signal.map Result.toMaybe
+
+
+eventDecoder =
+    object3 Event
+                ("timestamp" := float)
+                ("pid"       := string)
+                ("type"      := string)
+
 
 port location : Maybe String
